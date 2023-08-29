f63a4d33fdd01a22e3f6a24f8f26e407a251bb9a
Lucifer4760 <132442928+Lucifer4760@users.noreply.github.com>
2023-08-29 18:29:26 +0530

    Add files via upload

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+  $('.about-info-inner').typed({
+    strings: [
+      "<span>Hi, I am Nitin Thakur</span> A third-year student at Chitkara University Punjab. Currently, pursuing my B Tech degree in Computer Science. I was born and brought up in Sirmaur, Himachal Pradesh.<br><br>Self-driven, quick starter, passionate programmer with a curious mind who enjoys solving complex and challenging real-world problems."
+    ],
+    typeSpeed: 7,
+    contentType: 'html'
+  });
+}, 100);
